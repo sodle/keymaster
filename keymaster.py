634a0bde16938b4ff78ac1b5bf09599471848ff5
Scott Odle <scott@sjodle.com>
2015-11-21 07:25:19 -0700

    flask routing

--- keymaster.py
+++ keymaster.py
@@ -1,9 +1,9 @@
 from flask import Flask
 application = Flask(__name__)
 
-@application.route('/k/asdf')
-def index():
-    return 'Hello key #'
+@application.route('/k/<key_id>')
+def index(key_id):
+    return 'Hello key #' + key_id
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
