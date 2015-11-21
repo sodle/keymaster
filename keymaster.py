d1eaebb9b09b284ffe6b9350f94edb8f862e0b55
Scott Odle <scott@sjodle.com>
2015-11-20 19:19:22 -0700

    change "app" to "application"

--- keymaster.py
+++ keymaster.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+applicationlication = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return 'Hello world!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    application.run(host='0.0.0.0')
