ade8dcf4cac41dc61b1590bb297bd25f787a1340
Scott Odle <scott@sjodle.com>
2015-11-21 21:06:40 -0700

    ORM

--- config.spec.py
+++ config.spec.py
@@ -9,3 +9,6 @@ BASE_SHORTENER_URL = 'http://key.io'
 # Return a connection to the Postgresql database
 def DB_CONNECTION():
     return psycopg2.connect(database='keymaster', user='zuul')
+
+# Random salt to use for hashids
+HASH_SALT = 'stay_puft'
