a006a6c5eea47baba2b7548ea6000829a7d740b5
Scott Odle <scott@sjodle.com>
2015-11-21 18:26:21 -0700

    config file

---
+++ config.spec.py
@@ -0,0 +1,11 @@
+import psycopg2
+
+# Base URL of the web server
+BASE_URL = 'http://keymaster.com'
+
+# Base URL for shortlinks (if enabled)
+BASE_SHORTENER_URL = 'http://key.io'
+
+# Return a connection to the Postgresql database
+def DB_CONNECTION():
+    return psycopg2.connect(database='keymaster', user='zuul')
