6842ddcdddf76bc5f56ab02d18c9d08c2a116d93
Scott Odle <scott@sjodle.com>
2015-11-22 19:31:04 -0700

    echo logs afterwards

--- tests.py
+++ tests.py
@@ -21,5 +21,4 @@ class KeymasterTests(unittest.TestCase):
 
     def test_key_upload(self):
         result = self.app.post('/k', data=dict(public_key="test key"))
-        print(result.status)
         self.assertEqual(result.status_code, 200)
