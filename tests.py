f90f505ea334a4002f507c14336161f030faa50a
Scott Odle <scott@sjodle.com>
2015-11-22 19:08:53 -0700

    test uploading keys

--- tests.py
+++ tests.py
@@ -1,2 +1,24 @@
 from wsgi import application
 import unittest
+
+
+class KeymasterTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        self.app = application.test_client()
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    def test_key_upload(self):
+        result = self.app.post('/k', data=dict(public_key="test key"))
+        self.assertEqual(result.status_code, 200)
